89df11bdd131fb02879ba9da5433a4756fb75bcc
kelbovin <110257377+kelbovin@users.noreply.github.com>
2023-01-12 11:01:05 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_7.1"
+export DT_LINK="https://github.com/seluce/android_device_cubot_rainbow.git -b fox_7.1"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="cubot"
+export OEM="cubot"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=KelGD"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
